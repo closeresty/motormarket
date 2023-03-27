51bda6b137ccef156b334b56022ed0afa0b82ef3
Resty Abarera Potolin <85859112+closeresty@users.noreply.github.com>
2023-03-27 14:40:57 +0800

    Add files via upload

---
+++ motorpartsmarket/components/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=id19347521_shop_db';
+$user_name = 'id19347521_root';
+$user_password = 'qmA8Yj~m3<@<pV[#';
+
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
